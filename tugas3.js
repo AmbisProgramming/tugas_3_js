629415e7e8c4e02589682fd71004201b98bb9176
AmbisProgramming <110365940+AmbisProgramming@users.noreply.github.com>
2022-08-01 12:50:54 +0700

    Add files via upload

---
+++ tugas3.js
@@ -0,0 +1,10 @@
+const decrement = (x) => {
+  x = x + 1;
+  return x;
+};
+
+let nilai = 10;
+console.log("Nilai sebelum dirubah : ");
+console.log(nilai);
+console.log("Nilai setelah dirubah : ");
+console.log(decrement(nilai));
